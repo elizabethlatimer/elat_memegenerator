4f0bd67b3fa0ef0605d9b42d1acc95895621db85
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2019-12-08 21:49:15 -0800

    js structuring

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-window.onload = function() {
+const createAndAppendDiv = (imgAddress, topText, bottomText) => {
+    let section = document.querySelector(".meme-holder")
 
+    let memeDiv = document.createElement("div");
+    memeDiv.className = "meme";
+
+    let topDiv = document.createElement("div");
+    topDiv.className = "meme-text-top";
+    topDiv.innerText = topText;
+
+    let bottomDiv = document.createElement("div");
+    bottomDiv.className = "meme-text-bottom";
+    bottomDiv.innerText = bottomText;
+
+    let image = document.createElement("img");
+    image.src = imgAddress;
+
+    let exit = document.createElement("div");
+    exit.className = "exit";
+    exit.innerText = "X"
+
+    memeDiv.appendChild(image);
+    memeDiv.appendChild(topDiv);
+    memeDiv.appendChild(bottomDiv);
+    memeDiv.appendChild(exit);
+    section.appendChild(memeDiv);
+
+    exit.addEventListener("click", function(){
+        memeDiv.remove();
+    })
+};
+
+window.onload = function() {
     let button = document.querySelector('.button');
     button.addEventListener ("click", function() {
         let imgAddress = document.getElementById("link").value;
@@ -9,36 +40,7 @@ window.onload = function() {
         let bottomText = document.getElementById("bottom-text").value;
         document.getElementById("bottom-text").value = "";
 
-        let section = document.querySelector(".meme-holder")
-
-        let memeDiv = document.createElement("div");
-        memeDiv.className = "meme";
-
-        let topDiv = document.createElement("div");
-        topDiv.className = "meme-text-top";
-        topDiv.innerText = topText;
-
-        let bottomDiv = document.createElement("div");
-        bottomDiv.className = "meme-text-bottom";
-        bottomDiv.innerText = bottomText;
-
-        let image = document.createElement("img");
-        image.src = imgAddress;
-
-        let exit = document.createElement("div");
-        exit.className = "exit";
-        exit.innerText = "X"
-
-        memeDiv.appendChild(image);
-        memeDiv.appendChild(topDiv);
-        memeDiv.appendChild(bottomDiv);
-        memeDiv.appendChild(exit);
-        section.appendChild(memeDiv);
-
-        exit.addEventListener("click", function(){
-            memeDiv.remove();
-        })
-
-    })
+        createAndAppendDiv(imgAddress, topText, bottomText);
+    });
 
 }
